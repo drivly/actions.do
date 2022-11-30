44ff64e8b6cdaaf1ce0c04e6725fecb5af2e3589
Nathan Clevenger <nateclev@gmail.com>
2022-11-30 05:10:20 -0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,18 +1,18 @@
 export const api = {
   icon: '🚀',
-  name: 'templates.do',
+  name: 'actions.do',
   description: 'Cloudflare Worker Template',
-  url: 'https://templates.do/api',
-  type: 'https://apis.do/templates',
+  url: 'https://actions.do/api',
+  type: 'https://apis.do/primitives',
   endpoints: {
-    listCategories: 'https://templates.do/api',
-    getCategory: 'https://templates.do/:type',
+    listCategories: 'https://actions.do/api',
+    getCategory: 'https://actions.do/:type',
   },
-  site: 'https://templates.do',
-  login: 'https://templates.do/login',
-  signup: 'https://templates.do/signup',
-  subscribe: 'https://templates.do/subscribe',
-  repo: 'https://github.com/drivly/templates.do',
+  site: 'https://actions.do',
+  login: 'https://actions.do/login',
+  signup: 'https://actions.do/signup',
+  subscribe: 'https://actions.do/subscribe',
+  repo: 'https://github.com/drivly/actions.do',
 }
 
 export const gettingStarted = [
@@ -21,7 +21,7 @@ export const gettingStarted = [
 ]
 
 export const examples = {
-  listItems: 'https://templates.do/worker',
+  listItems: 'https://actions.do/worker',
 }
 
 export default {
